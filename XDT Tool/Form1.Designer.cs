55803ee090e11b7775a80d0b4ba28e8e713cf34e
Raf Van Hoof <raf.vanhoof@gmail.com>
2017-01-27 18:31:39 +0100

    Added Windows client xdt tool

---
+++ XDT Tool/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace XDT_Tool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.diffToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.textBoxSource = new System.Windows.Forms.TextBox();
+            this.textBoxTarget = new System.Windows.Forms.TextBox();
+            this.textBoxXDT = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 28);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1010, 681);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.textBoxSource);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1002, 652);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Source XML";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textBoxTarget);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1002, 652);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Target XML";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.diffToolStripMenuItem,
+            this.clearToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1010, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // diffToolStripMenuItem
+            // 
+            this.diffToolStripMenuItem.Name = "diffToolStripMenuItem";
+            this.diffToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.diffToolStripMenuItem.Text = "Diff";
+            this.diffToolStripMenuItem.Click += new System.EventHandler(this.diffToolStripMenuItem_Click);
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(55, 24);
+            this.clearToolStripMenuItem.Text = "Clear";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.clearToolStripMenuItem_Click);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.textBoxXDT);
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(1002, 652);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "XDT";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // textBoxSource
+            // 
+            this.textBoxSource.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxSource.Location = new System.Drawing.Point(3, 3);
+            this.textBoxSource.Multiline = true;
+            this.textBoxSource.Name = "textBoxSource";
+            this.textBoxSource.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxSource.Size = new System.Drawing.Size(996, 646);
+            this.textBoxSource.TabIndex = 0;
+            // 
+            // textBoxTarget
+            // 
+            this.textBoxTarget.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxTarget.Location = new System.Drawing.Point(3, 3);
+            this.textBoxTarget.Multiline = true;
+            this.textBoxTarget.Name = "textBoxTarget";
+            this.textBoxTarget.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxTarget.Size = new System.Drawing.Size(996, 646);
+            this.textBoxTarget.TabIndex = 1;
+            // 
+            // textBoxXDT
+            // 
+            this.textBoxXDT.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxXDT.Location = new System.Drawing.Point(0, 0);
+            this.textBoxXDT.Multiline = true;
+            this.textBoxXDT.Name = "textBoxXDT";
+            this.textBoxXDT.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxXDT.Size = new System.Drawing.Size(1002, 652);
+            this.textBoxXDT.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1010, 709);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "XDT Tool";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem diffToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBoxSource;
+        private System.Windows.Forms.TextBox textBoxTarget;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TextBox textBoxXDT;
+    }
+}
+
---
+++ XDT Tool/Form1.cs
@@ -0,0 +1,84 @@
+﻿using FatAntelope;
+using FatAntelope.Writers;
+using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace XDT_Tool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void diffToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var source = new XmlDocument();
+                source.LoadXml(textBoxSource.Text);
+                var target = new XmlDocument();
+                target.LoadXml(textBoxTarget.Text);
+
+                var tree1 = new XTree(source);
+                var tree2 = new XTree(target);
+
+
+                XDiff.Diff(tree1, tree2);
+                if (tree1.Root.Match == MatchType.Match && tree2.Root.Match == MatchType.Match && tree1.Root.Matching == tree2.Root)
+                {
+                    Message("Warning: No difference found!\n");                    
+                }
+
+                if (tree1.Root.Match == MatchType.NoMatch || tree2.Root.Match == MatchType.NoMatch)
+                {
+                    Message("Error: Root nodes must have the same name!\n");                  
+                }
+             
+                var writer = new XdtDiffWriter();
+                var patch = writer.GetDiff(tree2);
+                textBoxXDT.Text = PrettyXml(patch.OuterXml);
+                tabControl1.SelectedIndex = 2;
+            }
+            catch (Exception ex)
+            {
+                Message(ex.ToString());                
+            }
+        }
+
+        static string PrettyXml(string xml)
+        {
+            var stringBuilder = new StringBuilder();
+
+            var element = XElement.Parse(xml);
+
+            var settings = new XmlWriterSettings();
+            settings.OmitXmlDeclaration = false;
+            settings.Indent = true;
+            settings.NewLineOnAttributes = true;
+
+            using (var xmlWriter = XmlWriter.Create(stringBuilder, settings))
+            {
+                element.Save(xmlWriter);
+            }
+
+            return stringBuilder.ToString();
+        }
+        
+        private void Message(string text)
+        {
+            MessageBox.Show(this, text);
+        }
+
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBoxSource.Text = null;
+            textBoxTarget.Text = null;
+            textBoxXDT.Text = null;
+        }
+    }
+}
